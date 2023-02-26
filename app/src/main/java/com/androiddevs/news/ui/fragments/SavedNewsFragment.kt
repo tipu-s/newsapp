f68212bf6b09765296d3bb74bcf8d4a92bedc1cd
Tipu Sultan <tipu.ru.cse@gmail.com>
2023-02-26 00:35:43 +0000

    delete item undo handle via flow pattern

--- app/src/main/java/com/androiddevs/news/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/androiddevs/news/ui/fragments/SavedNewsFragment.kt
@@ -2,7 +2,9 @@ package com.androiddevs.news.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -14,6 +16,7 @@ import com.androiddevs.news.R
 import com.androiddevs.news.adapter.NewsAdapter
 import com.androiddevs.news.api.RetrofitInstance
 import com.androiddevs.news.db.ArticleDatabase
+import com.androiddevs.news.model.Article
 import com.androiddevs.news.repository.NewsRepository
 import com.androiddevs.news.ui.viewModel.SavedNewsViewModel
 import com.androiddevs.news.ui.viewModel.SavedNewsViewModelProviderFactory
@@ -44,6 +47,7 @@ class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
             )
         }
 
+        lateinit var deletedArticle: Article
         val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
             ItemTouchHelper.UP or ItemTouchHelper.DOWN,
             ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
@@ -58,14 +62,8 @@ class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                val article = newsAdapter.differ.currentList[position]
-                viewModel.deleteArticle(article)
-                Snackbar.make(view, "Deleted article", Snackbar.LENGTH_LONG).apply {
-                    setAction("Undo") {
-                        viewModel.insertArticle(article)
-                    }
-                    show()
-                }
+                deletedArticle = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(deletedArticle)
             }
         }
 
@@ -73,7 +71,18 @@ class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
             attachToRecyclerView(rvSavedNews)
         }
 
+        viewModel.dbDeleteSuccess.observe(viewLifecycleOwner, Observer {
+            Snackbar.make(view, "Deleted article", Snackbar.LENGTH_LONG).apply {
+                setAction("Undo") {
+                    viewModel.insertArticle(deletedArticle)
+                }
+                show()
+            }
+        })
 
+        viewModel.dbInsertError.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(activity, "$it", Toast.LENGTH_SHORT).show()
+        })
 
         lifecycleScope.launch {
             viewModel.savedArticle.collectLatest {
